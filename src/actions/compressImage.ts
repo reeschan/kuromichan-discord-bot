ee45f5cedb1990b96d62842a3cbd4e72e8a09176
reeschan <t0gah1m1k0@kur1su-amad3us.tokyo>
2024-11-03 23:13:11 +0900

    [Add][Update]イメージ圧縮コマンドの追加

---
+++ src/actions/compressImage.ts
@@ -0,0 +1,19 @@
+import { JsonResponse } from '../types';
+import { InteractionResponseType } from 'discord-interactions';
+
+export async function compressImage(interaction: any, env: Env) {
+	const attachmentId = Object.keys(interaction.data.resolved.attachments)[0];
+	const attachment = interaction.data.resolved.attachments[attachmentId];
+	const imageUrl = attachment.url;
+	const imageResponse = await fetch(imageUrl);
+	const imageArrayBuffer = await imageResponse.arrayBuffer();
+	const base64Image = Buffer.from(imageArrayBuffer).toString('base64');
+	const quality = interaction.data?.options?.[1]?.value ?? 80 * 1000;
+	const result = await env.COMPRESS_IMAGE.compressImage(base64Image, quality);
+
+	// Base64でもとにかく返すんだよ！！！！！！！！！！！ｗｗｗｗｗ discordはbase64でurl添付できないから・・・
+	return new JsonResponse({
+		type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+		data: { content: result },
+	});
+}
