f0255dde63fab4afadab4501187b10c10d5b54d5
reeschan <t0gah1m1k0@kur1su-amad3us.tokyo>
2024-11-04 00:46:44 +0900

    [Fix] export の修正

--- workers/image-compresser/src/index.ts
+++ workers/image-compresser/src/index.ts
@@ -1,7 +1,7 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
 import { compress_image } from './external/wasm/compress-image';
 
-export default class ImageCompresser extends WorkerEntrypoint {
+export class ImageCompresser extends WorkerEntrypoint {
 	async compressImage(image: string, size: number): Promise<string> {
 		return new Promise((resolve, reject) => {
 			try {
@@ -12,3 +12,9 @@ export default class ImageCompresser extends WorkerEntrypoint {
 		});
 	}
 }
+
+export default {
+	fetch() {
+		return new Response('HelloRpc is Healthy!');
+	},
+};
